4951b29c5361c5f22787a377217c4b37308f58c1
Lucky Amadi <lucky@softcom.ng>
2020-04-06 05:55:19 +0100

    added sass styling for the app grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import Homepage from "./components/Homepage.component";
+import Homepage from "./components/homepage/Homepage.component";
 
 function App() {
 	return (
