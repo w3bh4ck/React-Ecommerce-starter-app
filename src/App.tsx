3042bbae74c048ec30e6948ce3a203ab72f3c766
Lucky Amadi <lucky@softcom.ng>
2020-04-07 08:25:17 +0100

    added styling to the menu item

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Homepage from "./pages/home-page/Homepage.component";
+import "./App.css";
 
 function App() {
 	return (
--- src/components/menu-item/MenuItem.component.tsx
+++ src/components/menu-item/MenuItem.component.tsx
@@ -9,13 +9,16 @@ interface IPROPS {
 const MenuItem = ({ title, imageUrl, size }: IPROPS) => {
 	return (
 		<>
-			<div
-				style={{
-					background: `url(${imageUrl})`,
-				}}
-				className={`${size} menu-item`}>
+			<div className={`${size} menu-item`}>
+				<div
+					className="background-image"
+					style={{
+						background: `url(${imageUrl})`,
+					}}
+				/>
+
 				<div className="content">
-					<h1 className="title">{title}</h1>
+					<h1 className="title">{title.toUpperCase()}</h1>
 					<span className="subtitle">Shop Now</span>
 				</div>
 			</div>
