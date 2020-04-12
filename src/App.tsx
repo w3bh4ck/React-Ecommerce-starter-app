b627a314e1e7dfeee6622b2df37068afe23030ca
Lucky Amadi <lucky@softcom.ng>
2020-04-12 10:31:48 +0100

    added setup  for the shop page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import React from "react";
+import { Route, Switch, BrowserRouter } from "react-router-dom";
 import Homepage from "./pages/home-page/Homepage.component";
 import "./App.css";
+import ShopPage from "./pages/shop-page/Shop.component";
 
 function App() {
 	return (
 		<div>
-			<Homepage />
+			<BrowserRouter>
+				<Switch>
+					<Route exact path="/" component={Homepage} />
+					<Route exact path="/shops" component={ShopPage} />
+				</Switch>
+			</BrowserRouter>
 		</div>
 	);
 }
