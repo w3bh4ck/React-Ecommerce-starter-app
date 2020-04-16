245d30db5a060ec1ec23a08162d0ea2e8100d08f
Lucky Amadi <lucky@softcom.ng>
2020-04-16 23:43:15 +0100

    connected app to firestore

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -13,6 +13,28 @@ const config = {
 	measurementId: "G-0CPEP0L7HM",
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+	if (!userAuth) return;
+	const userRef = firestore.doc(`users/${userAuth}`);
+	const snapshot = await userRef.get();
+	if (snapshot.exists) {
+		const { displayName, email } = userAuth;
+		const createdAt = new Date();
+		try {
+			await userRef.set({
+				displayName,
+				email,
+				createdAt,
+				...additionalData,
+			});
+		} catch (error) {
+			console.log("error creating user");
+		}
+	}
+
+	return userRef;
+};
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
