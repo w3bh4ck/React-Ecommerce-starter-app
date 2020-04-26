ecc28768f926bb65c6a4b21384ac56d536f15cdb
Lucky Amadi <lucky@softcom.ng>
2020-04-26 21:19:41 +0100

    remove items from checkout

--- src/components/checkout-item/CheckoutItem.component.tsx
+++ src/components/checkout-item/CheckoutItem.component.tsx
@@ -1,18 +1,22 @@
 import React from "react";
 import "./CheckoutItem.styles.scss";
+import { connect } from "react-redux";
+import { clearItemFromCart } from "../../redux/cart/cart.action";
 
-const CheckoutItem = ({ cartItem }) => {
+const CheckoutItem = ({ cartItem, clearItemFromCart }) => {
 	return (
 		<div className="checkout-item">
 			<div className="image-container">
 				<img src={cartItem.imageUrl} alt="item" />
 			</div>
 			<span className="name">{cartItem.name}</span>
-			<span className="quantity">{cartItem.quantity}</span>
 			<span className="price">{cartItem.price}</span>
-			<span className="remove-button">&#10005;</span>
+			<span className="quantity">{cartItem.quantity}</span>
+			<span onClick={() => clearItemFromCart(cartItem)} className="remove-button">
+				&#10005;
+			</span>
 		</div>
 	);
 };
 
-export default CheckoutItem;
+export default connect(null, { clearItemFromCart })(CheckoutItem);
