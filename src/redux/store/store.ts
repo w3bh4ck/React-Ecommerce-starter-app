5c91e2f4251a1bd60ed063a0357eae8b6f45e961
Lucky Amadi <lucky@softcom.ng>
2020-04-27 01:06:56 +0100

    persistina app state with redux persist.

--- src/redux/store/store.ts
+++ src/redux/store/store.ts
@@ -1,10 +1,11 @@
 import { createStore, applyMiddleware } from "redux";
+import { persistStore } from "redux-persist";
 import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
 import rootReducer from "../reducers/root-reducer";
 
 const middleWare = [thunk];
 
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(...middleWare)));
+export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(...middleWare)));
 
-export default store;
+export const persistor = persistStore(store);
