b0e3e7af63591c8f9b1a77612e8951e4d1574933
Amadi Lucky Sampson <amadiluckysampson@gmail.com>
2020-10-02 10:16:11 +0100

    Update firebase.config.js

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,10 +1,10 @@
 export const config = {
-	apiKey: "AIzaSyCnhxQ_aLIBKi833Xe1qHUFvIpq7zolBDw",
-	authDomain: "e-commerce-db-596bb.firebaseapp.com",
-	databaseURL: "https://e-commerce-db-596bb.firebaseio.com",
-	projectId: "e-commerce-db-596bb",
-	storageBucket: "e-commerce-db-596bb.appspot.com",
+	apiKey: "",
+	authDomain: "",
+	databaseURL: "",
+	projectId: "",
+	storageBucket: "",
 	messagingSenderId: "200994850967",
-	appId: "1:200994850967:web:8fa2e2954770f7cfa0c1d2",
+	appId: "",
 	measurementId: "G-0CPEP0L7HM",
 };
